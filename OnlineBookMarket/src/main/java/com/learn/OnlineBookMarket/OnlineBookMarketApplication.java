e4ab0ff5ae9ea7c381f3aa5e47bade68494138af
Sagar r <sagaryuraj@gmail.com>
2020-03-07 11:42:57 +0530

    initial commit.spring boot backend project created and application property edited

---
+++ OnlineBookMarket/src/main/java/com/learn/OnlineBookMarket/OnlineBookMarketApplication.java
@@ -0,0 +1,13 @@
+package com.learn.OnlineBookMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookMarketApplication.class, args);
+	}
+
+}
---
+++ OnlineBookMarket/src/test/java/com/learn/OnlineBookMarket/OnlineBookMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.OnlineBookMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
