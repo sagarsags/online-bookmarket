c80639cc55fcaac2f611a3f78dc20d68fbb8c783
Sagar r <sagaryuraj@gmail.com>
2020-03-11 01:06:41 +0530

    angular added and feature 1 completed

---
+++ angular-bookmarket/src/app/common/book.ts
@@ -0,0 +1,15 @@
+export class Book {
+
+     sku :string;
+     name:string;
+     description:string;
+     unitPrice:number;
+     imageUrl:string;
+     unitsInStock:number;
+     active:boolean;
+     dateCreated:Date;
+     lastUpdated:Date;
+
+}
+
+
