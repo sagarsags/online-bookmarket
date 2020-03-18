2fbad0d5c58336cb5bdf699c5ff1113b9ce212c1
Sagar r <sagaryuraj@gmail.com>
2020-03-19 00:56:33 +0530

    feature angular files update

--- angular-bookmarket/src/app/components/book-list/book-list.component.ts
+++ angular-bookmarket/src/app/components/book-list/book-list.component.ts
@@ -4,7 +4,8 @@ import { BookService } from 'src/app/service/book.service';
 
 @Component({
   selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
+  //templateUrl: './book-list.component.html',
+  templateUrl: './book-grid.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
