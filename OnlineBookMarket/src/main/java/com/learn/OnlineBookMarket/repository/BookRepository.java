df642cb0573ef8d8cbcabf6fb1537e60c6ac76d2
Sagar r <sagaryuraj@gmail.com>
2020-03-23 14:51:08 +0530

    repository file updated through notepad++

--- OnlineBookMarket/src/main/java/com/learn/OnlineBookMarket/repository/BookRepository.java
+++ OnlineBookMarket/src/main/java/com/learn/OnlineBookMarket/repository/BookRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.learn.OnlineBookMarket.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
+    
+	@RestResource(path="categoryId")
+	Page<Book> findByCategoryId(@Param("id") long id,Pageable pageable);
+} 
