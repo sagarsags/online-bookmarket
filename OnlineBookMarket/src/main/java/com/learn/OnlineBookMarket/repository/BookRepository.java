96c628d3f24a269b925e93c7947876c07bd128a4
Sagar r <sagaryuraj@gmail.com>
2020-03-31 15:17:59 +0530

    feature 5 java change

--- OnlineBookMarket/src/main/java/com/learn/OnlineBookMarket/repository/BookRepository.java
+++ OnlineBookMarket/src/main/java/com/learn/OnlineBookMarket/repository/BookRepository.java
@@ -8,4 +8,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     
 	@RestResource(path="categoryId")
 	Page<Book> findByCategoryId(@Param("id") long id,Pageable pageable);
+	
+	@RestResource(path="searchBook")
+	Page<Book> findByNameContaining(@Param("name") String name,Pageable pageable);
 } 
